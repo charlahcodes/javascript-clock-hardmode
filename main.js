8cfba0ac5861fdb64a3ccc67bd2ed6c0fe1f5ae1
Charlie Miller <charlie@thecharlah.com>
2015-10-05 17:40:34 -0400

    Removed extra JS

--- main.js
+++ main.js
@@ -7,24 +7,13 @@ var intervalID = window.setInterval(function displayClock() {
   var minutes = newDate.getMinutes();
   var seconds = newDate.getSeconds();
 
-  if (hours < 10) {
-    hours = "0" + hours;
-  };
-
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  };
-
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  };
-
-
   var hoursHex = hours.toString(16);
   var minutesHex = minutes.toString(16);
   var secondsHex = seconds.toString(16);
 
-  var time = hoursHex + '' + minutesHex + '' + secondsHex;
+  var time = hoursHex + minutesHex + secondsHex;
+
+  console.log(time);
 
   var backgroundHex = "#"+time;
 
